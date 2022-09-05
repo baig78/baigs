38d184bca7331a8be12776a47a9c3be403843b2e
baig78 <mirzanaimathbaig@gmail.com>
2022-09-05 15:46:25 +0530

    components added

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,11 +3,25 @@ import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { DeleteProductComponent } from './delete-product/delete-product.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
+import { ViewAllProductComponent } from './view-all-product/view-all-product.component';
+import { ViewProductComponent } from './view-product/view-product.component';
+import { ViewProductByCategoryComponent } from './view-product-by-category/view-product-by-category.component';
+import { ViewProductByDateComponent } from './view-product-by-date/view-product-by-date.component';
 
 
 @NgModule({
   declarations: [
-    ProductsComponent
+    ProductsComponent,
+    AddProductComponent,
+    DeleteProductComponent,
+    UpdateProductComponent,
+    ViewAllProductComponent,
+    ViewProductComponent,
+    ViewProductByCategoryComponent,
+    ViewProductByDateComponent
   ],
   imports: [
     CommonModule,
